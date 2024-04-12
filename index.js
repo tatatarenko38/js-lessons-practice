d47f036f6493e94d703e2130f069fcad75880cc8
Татьяна Татаренко <tatatatarenko38@gmail.com>
2024-04-12 12:05:10 +0300

    patern1

--- index.js
+++ index.js
@@ -2313,42 +2313,398 @@
 
 //////        Фабрика    Factory   ///////
 
-class Button {
-  constructor({ text, color }) {
-    this.text = text;
-    this.color = color;
+// class Button {
+//   constructor({ text, color }) {
+//     this.text = text;
+//     this.color = color;
+//   }
+
+//   render() {
+//     return `<button class="color:${this.color};">${this.text}</button>`;
+//   }
+// }
+
+// class IconButton {
+//   constructor({ icon, color }) {
+//     this.icon = icon;
+//     this.color = color;
+//   }
+
+//   render() {
+//     return `<button class="color:${this.color};"><img src="${this.icon}"/></button>`;
+//   }
+// }
+
+// class ButtonFactory {
+//   static TYPE = {
+//     BASIC: "basic",
+//     ICON: "icon",
+//   };
+
+//   static createButton(type, options) {
+
+// if(options.icon){
+//   return new IconButton(options);
+// }
+
+// if(options.text){
+//   return new Button(options);
+// }
+// throw new Error(`Такого типу кнопки не існує: ${type}`);
+
+// // abo
+
+//     // switch (type) {
+//     //   case this.TYPE.BASIC:
+//     //     return new Button(options);
+//     //   case this.TYPE.ICON:
+//     //     return new IconButton(options);
+//     //   default:
+//     //     throw new Error(`Такого типу кнопки не існує: ${type}`);
+//     // }
+//   }
+// }
+// const myIconButton = ButtonFactory.createButton(ButtonFactory.TYPE.ICON, {
+//   color: "red",
+//   icon: "/icon/my-icon.svg,",
+// });
+// console.log(myIconButton);
+
+///  Спостерігач     Один - багато  Observer  ///////////
+
+// наприклад, створюється одне відео на каналі, повідомлення з його назвою приходить
+// //всім користувачам
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   sendEmail(message) {
+//     console.log(`Відправити на email ${this.email} повідомлення: ${message}`);
+//   }
+// }
+
+// class Video {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// class Channel {
+//   constructor(name){
+//     this.name = name;
+//     this.subscribers = [];
+//   }
+
+//   subscribe(user){
+//     // підписка на каналі
+//     this.subscribers.push(user);
+//   }
+
+//   unsubscribe(user){
+//     // відписка від каналу
+//     this.subscribers = this.subscribers.filter((sub) => sub !== user);
+//   }
+
+//   createVideo(name){
+//     //створення нового відео
+//     const video = new Video(name);
+//     this.sendNotify(video);
+//   }
+
+//   sendNotify(video){
+//     // відправка повідомлення підписникам про нове відео
+//     this.subscribers.forEach((subscriber) => {
+//       subscriber.sendEmail(`Нове відео "${video.name}" на Youtube каналі ${this.name}!`);
+//     });
+//   }
+// }
+
+// const channel = new Channel("IT Brains");
+
+// const user1 = new User("jhon@example.com");
+// const user2 = new User("eva@example.com");
+// const user3 = new User("olga@example.com");
+
+// channel.subscribe(user1);
+// channel.subscribe(user2);
+// channel.subscribe(user3);
+
+// channel.createVideo("Урок по JS")
+
+// //відписати з каналу
+// channel.unsubscribe(user2);
+
+// channel.createVideo("Урок по CSS");
+
+// Decorator  -розширює функціональність не змінюючи сам об'єкт  //
+
+// //кавовий апарат з різними напоями
+// class Coffee {
+//   name = "Кава";
+
+//   cost = 10;
+
+//   cook(){
+//     (`Приготування ${this.name}`);
+//     // Логіка приготування кавового напою
+//   }
+// }
+
+// class MilkDecorator{
+//   constructor(coffee, amount){
+//     this.coffee = coffee;
+//     this.amount = amount;
+//   }
+
+//   get name(){
+//     return `${this.coffee.name}, з ${this.amount}мл молока`
+//   }
+//   get cost(){
+//     const milkPrice = 0.05;
+//     return this.coffee.cost + milkPrice * this.amount;
+//   }
+
+//   cook(){
+//     console.log(`Приготування ${this.name}`);
+//     //Логіка приготування кави з молоком
+//   }
+// }
+//   // створення об'єкту базової кави
+//   let coffee = new Coffee();
+//   console.log(coffee.name);
+//   console.log(coffee.cost);
+//   coffee.cook();
+
+//   //Додавання декоротора молока до кави
+//   let latteCoffee = new MilkDecorator(coffee,300);
+//   console.log(latteCoffee.name);
+//   console.log(latteCoffee.cost);
+//   latteCoffee.cook();
+
+// Memento
+//текстовий редактор
+
+// class TextEditor{
+//   #text = "";
+
+//   set text(text) {
+//     this.#text = text;
+//     this.#save();
+//   }
+
+//   get text(){
+//     return this.#text;
+//   }
+
+//   #save(){
+//     Snapshot.create(this.text);
+//   }
+
+//   restore(){
+//     this.#text = Snapshot.restore().text;
+//   }
+// }
+
+// class Snapshot{
+//   constructor(text){
+//     this.text = text;
+//   }
+
+//   static #snapshots = [];
+
+//   static create(text){
+//     console.log(text);
+//     this.#snapshots.push(new Snapshot(text));
+//   }
+
+//   static restore(){
+//     this.#snapshots.pop();
+//     return this.#snapshots[this.#snapshots.length - 1];
+//   }
+// }
+
+// const editor = new TextEditor();
+
+// editor.text = "Це початковий текст.";
+// editor.text = "Редагований текст.";
+// editor.text = "Оновлений текст.";
+
+// console.log("===");
+
+// console.log(editor.text);
+
+// console.log("===");
+
+// editor.restore();
+// console.log(editor.text);
+
+// console.log("===");
+
+// editor.restore();
+// console.log(editor.text);
+
+///   Ланцюжок відповідальності   //
+//Базовий клас Обробник авторизації
+
+// class AuthHandler {
+//   // технічний клас, дозволяє далі по ланцюжку робити обробку
+//   setNextHandler(handler) {
+//     this.nextHandler = handler;
+//     return handler;
+//   }
+
+//   login(user, password) {
+//     if (this.nextHandler) {
+//       return this.nextHandler.login(user, password);
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+
+// class TwoFactorAuthHandler extends AuthHandler {
+//   login(user, password) {
+//     if (
+//       user === "jhon" &&
+//       password === "password" &&
+//       this.isValidTwoFactorCode()
+//     ) {
+//       console.log("Вхід дозволено з двофакторною автенфікацією");
+//       return true;
+//     } else {
+//       return super.login(user, password);
+//     }
+//   }
+
+//   isValidTwoFactorCode() {
+//     return true;
+//   }
+// }
+
+// const handler = new TwoFactorAuthHandler();
+
+// handler.setNextHandler({
+//   login: (login, password) => {
+//     const result =
+//       login === "login" && password == "password"
+//         ? "Користувач увійшов в акаунт"
+//         : "Користувач не увійшов в акакунт";
+//     console.log(result);
+//     return result;
+//   },
+// });
+
+// handler.login("login", "password");
+
+// class RoleHandler extends AuthHandler {
+//   login(user, password) {
+//     if (user === "quest") {
+//       console.log("Вхід дозволено в ролі гостя");
+//       return true;
+//     } else {
+//       return super.login(user, password);
+//     }
+//   }
+// }
+
+// class CredentialHandler extends AuthHandler {
+//   login(user, password) {
+//     if (user === "admin" && password === "admin123") {
+//       console.log("Вхід дозволено за логіном та паролем");
+//       return true;
+//     } else {
+//       return super.login(user, password);
+//     }
+//   }
+// }
+
+// const handler1 = new TwoFactorAuthHandler();
+
+// const handler2 = new CredentialHandler();
+
+// handler2.setNextHandler(new RoleHandler());
+
+// handler1.setNextHandler(handler2);
+
+// handler1.login("admin", "admin123");
+
+// //  для зручності можна створити окремий клас
+
+// class HandlerBuilder{
+//   constructor(){
+//     this.firstHandler = null;
+//     this.lastHandler = null;
+//   }
+
+//   add(handler){
+//     if(!this.firstHandler){
+//       this.firstHandler = handler;
+//       this.lastHandler = handler;
+//     } else {
+//       this.lastHandler.setNextHandler(handler);
+//       this.lastHandler = handler;
+//     }
+//     return this;
+//   }
+
+//   create(){
+//     return this.firstHandler
+//   }
+// }
+
+// const handlerBuilder = new HandlerBuilder();
+
+// const handler3 = handlerBuilder
+// .add(new CredentialHandler())
+// .add(new TwoFactorAuthHandler())
+// .add(new RoleHandler())
+// .create()
+
+// handler3.login("admin", "admin123"); //Вхід дозволено за логіном та паролем
+
+// handler3.login("jhon", "password"); //Вхід дозволено з двофакторною
+
+// handler3.login("quest", "quest123"); //Вхід дозволено в ролі гостя
+
+// handler3.login("user", "password"); //Вхід заборонено
+
+
+///  МІСТ  ///
+
+// відпрака може бути через смс, емаіл, телеграм(messeger)
+class User{
+  constructor(name, messager){
+    this.name = name;
+    this.messager = messager;
   }
 
-  render() {
-    return `<button class="color:${this.color};">${this.text}</button>`;
+  sendMessege(message) {
+    const formattedMessege = this.formatMessege(message);
+    this.messager.sendMessege(formattedMessege);
+    return formattedMessege;
+  }
+
+  formatMessege(message){
+    return `[${this.name}] : ${message}`;
   }
 }
 
-class IconButton {
-  constructor({ icon, color }) {
-    this.icon = icon;
-    this.color = color;
-  }
-
-  render() {
-    return `<button class="color:${this.color};"><img src="${this.icon}"/></button>`;
+class SMSMesseger{
+  static sendMessege(message){
+    console.log(`Відправлено SMS: ${message}`);
   }
 }
 
-class ButtonFactory {
-  static TYPE = {
-    BASIC: "basic",
-    ICON: "icon",
-  };
-
-  static createButton(type, options) {
-    switch (type) {
-      case this.TYPE.BASIC:
-        return new Button(options);
-      case this.TYPE.ICON:
-        return new IconButton(options);
-      default:
-        throw new Error(`Такого типу кнопки не існує: ${type}`);
-    }
+class EmailMesseger{
+  static sendMessege(message){
+    console.log(`Відправлено Email: ${message}`);
   }
 }
+
+const john = new User("John", SMSMesseger);
+const jane = new User("Jane", EmailMesseger);
+
+john.sendMessege("Привіт!"); // Відправлено SMS: [John]: Привіт!
+jane.sendMessege("Привіт!"); // Відправлено Email: [Jane]: Привіт!
